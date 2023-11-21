1f6f5ece4bc15a8b904de3cc8f0e1bdffe5329c0
alexander <alexander@klippel.se>
2023-11-21 21:41:39 +0100

    sample config

---
+++ config_sample.py
@@ -0,0 +1,22 @@
+TAUTULLI_SERVERS = [
+    {
+        'name': 'server1',
+        'url': 'http://server',
+        'api_key': 'API_KEY'
+    },
+    {
+        'name': 'server2',
+        'url': 'http://server',
+        'api_key': 'API_KEY'
+    },
+    {
+        'name': 'server3',
+        'url': 'http://server',
+        'api_key': 'API_KEY'
+    },
+    {
+        'name': 'server4',
+        'url': 'v',
+        'api_key': 'API_KEY'
+    },
+]
